27d7730d3687108c874532166983316e4bb63f41
Thanhung0206 <hungnguyenthanh2006@gmail.com>
2025-07-07 17:30:53 +0700

    code

---
+++ Madlib.py
@@ -0,0 +1,13 @@
+noun = input("Which language: ")
+noun2 = input("When: ")
+noun3 = input("Sport: ")
+noun4 = input("Passion: ")
+adj = input("Felling: ")
+verb = input("Verb: ")
+
+madlibs= f" I like programming in {noun}, so I work in the morning, in the afternoon, and in the {noun2}. In my free time, \
+I often play {noun3} and {noun4} with my friends to {adj}. After that, I come home and {verb} programming. "
+
+print(madlibs)
+        
+    
